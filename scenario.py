b3009062d2d752365b69dd9bc5ed391126d5913c
HSZemi <hszemi@users.noreply.github.com>
2019-10-19 23:29:38 +0200

    Resize the map and put storm on the outside

--- scenario.py
+++ scenario.py
@@ -3,6 +3,28 @@
 from agescx import *
 import sys
 
+def place_rectangle(distance, spacing, object_to_place):
+	# southwest
+	x = distance
+	for y in range(distance, height-distance, spacing):
+		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=object_to_place)
+
+	# northeast
+	x = width-distance-1
+	for y in range(distance, height-distance, spacing):
+		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=object_to_place)
+
+	# northwest
+	y = distance
+	for x in range(distance+spacing, width-distance-1, spacing):
+		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=object_to_place)
+
+	# southeast
+	y = height-distance-1
+	for x in range(distance+spacing, width-distance-1, spacing):
+		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=object_to_place)
+	
+
 if(len(sys.argv) < 2):
 	print('Usage: ./{} <scx file>'.format(sys.argv[0]))
 	sys.exit()
@@ -18,128 +40,63 @@ scenario = Scenario(inputfile)
 width = scenario.tiles.width
 height = scenario.tiles.height
 
+print('Scenario size: {} x {}'.format(width, height))
+
+scenario.map.resize(width+20, height+20)
+
+# move old map into the new center
+for w in range(width-1, -1, -1):
+	for h in range(height-1, -1, -1):
+		oldPosition = scenario.map.tiles._tiles[w][h]
+		scenario.map.tiles._tiles[w+10][h+10] = Tile(row=w+10, col=h+10, type=oldPosition.type, elevation=oldPosition.elevation)
+
+for unit in scenario.units:
+	unit.move(10,10)
+
+width = scenario.tiles.width
+height = scenario.tiles.height
+
+
 relic = 285
 rock = 623
 torch = 499
 deepwater = 22
+beach = 37
 countdowns = [
 	303,  #/* seagulls */ 
 	333, #/* fakedeer */
 	862  #/* stormydog */
 	]
 
-# water it down
+# water around the outside
 
-# southwest
 areas = [
-	scenario.tiles.getArea(0,0,width-1,1),
-	scenario.tiles.getArea(0,height-2,width-1,height-1),
-	scenario.tiles.getArea(0,0,1,height-1),
-	scenario.tiles.getArea(width-2,0,width-1,height-1)
+	scenario.tiles.getArea(0,0,width-1,9),
+	scenario.tiles.getArea(0,height-10,width-1,height-1),
+	scenario.tiles.getArea(0,0,9,height-1),
+	scenario.tiles.getArea(width-10,0,width-1,height-1)
 	]
 for area in areas:
 	for tile in area:
-		tile.type = deepwater
+		if (tile.x + tile.y) % 2 == 0:
+			tile.type = deepwater
+		else:
+			tile.type = beach
 
 # relics
 for i in range(50):
 	scenario.units.new(x=0.5,y=0.5, owner=0, type=relic)
 
-# spawners and rocks
+#spawners
+for unit in countdowns:
+	place_rectangle(0, 5, unit)
 
-# southwest
-x = 0
-for y in range(0, height, 5):
-	for unit in countdowns:
-		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=unit)
-for y in range(1,height-1):
-	scenario.units.new(x=x+1.5,y=y+0.5, owner=0, type=rock)
-
-# northeast
-x = width-1
-for y in range(0, height, 5):
-	for unit in countdowns:
-		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=unit)
-for y in range(1,height-1):
-	scenario.units.new(x=x-0.5,y=y+0.5, owner=0, type=rock)
-
-# northwest
-y = 0
-for x in range(5, width-1, 5):
-	for unit in countdowns:
-		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=unit)
-for x in range(2,width-2):
-	scenario.units.new(x=x+0.5,y=y+1.5, owner=0, type=rock)
-
-# southeast
-y = height-1
-for x in range(5, width-1, 5):
-	for unit in countdowns:
-		scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=unit)
-for x in range(2,width-2):
-	scenario.units.new(x=x+0.5,y=y-0.5, owner=0, type=rock)
-
+# rocks
+place_rectangle(9, 1, rock)
 
 # torches
-
-# southwest
-x = 10
-for y in range(10, height-10, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# northeast
-x = width-10
-for y in range(11, height-10, 13):
-	scenario.units.new(x=x-0.5,y=y+0.5, owner=0, type=torch)
-
-# northwest
-y = 10
-for x in range(10, width-11, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# southeast
-y = height-10
-for x in range(11, width-11, 13):
-	scenario.units.new(x=x+0.5,y=y-0.5, owner=0, type=torch)
-
-# southwest
-x = 20
-for y in range(20, height-21, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# northeast
-x = width-20
-for y in range(20, height-21, 13):
-	scenario.units.new(x=x-0.5,y=y+0.5, owner=0, type=torch)
-
-# northwest
-y = 20
-for x in range(21, width-21, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# southeast
-y = height-20
-for x in range(21, width-21, 13):
-	scenario.units.new(x=x+0.5,y=y-0.5, owner=0, type=torch)
-
-# southwest
-x = 30
-for y in range(30, height-31, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# northeast
-x = width-30
-for y in range(30, height-31, 13):
-	scenario.units.new(x=x-0.5,y=y+0.5, owner=0, type=torch)
-
-# northwest
-y = 30
-for x in range(31, width-31, 13):
-	scenario.units.new(x=x+0.5,y=y+0.5, owner=0, type=torch)
-
-# southeast
-y = height-30
-for x in range(31, width-31, 13):
-	scenario.units.new(x=x+0.5,y=y-0.5, owner=0, type=torch)
+for distance in [10, 20, 30]:
+	d = distance + 10
+	place_rectangle(d, 13, torch)
 
 scenario.save(inputfile[:-4] + '_patched.scx')
