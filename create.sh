2ca8b3648edcdb73efcbfacbc982d6d8c81c5d18
HSZemi <hszemi@users.noreply.github.com>
2019-10-21 22:03:05 +0200

    Set final file names

--- create.sh
+++ create.sh
@@ -8,10 +8,10 @@ for i in `ls output/**/RR3*.scx | grep -v 'patched'`; do ./scenario.py $i; done
 
 rm output/**/ZR@*
 
-for i in output/*; do zip -0 -j $i/ZR@T`basename $i`.rms $i/`basename $i`.rms $i/`basename $i`_patched.scx; done
+for i in output/*; do zip -0 -j $i/ZR@`basename $i`.rms $i/`basename $i`.rms $i/`basename $i`_patched.scx; done
 
-rm -rf RR3_ALPHA
+rm -rf RR3
 
-mkdir -p RR3_ALPHA
+mkdir -p RR3
 
-cp output/**/ZR@T* ./RR3_ALPHA/
+cp output/**/ZR@* ./RR3/
